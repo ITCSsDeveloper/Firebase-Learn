22e4dbee3c65629db8dd1d73d7cd123ea3e6f6b9
ITCS'sDeveloper <itcss.develop.me@gmail.com>
2017-07-22 23:46:05 +0700

    npm install --save firebase-admin

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'); // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+// exports.date = functions.https.onRequest((req, res) => {
+//     if (req.method === 'POST') {
+//         res.status(403).send('Forbidden!');
+//     }
 // });
+
+console.log("test")

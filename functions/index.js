61fb05e3acc149ba5ff6c3ae24a5b6e5f1eaa278
ITCS'sDeveloper <itcss.develop.me@gmail.com>
2017-07-23 01:02:44 +0700

    Implement REST API

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,29 @@
 const functions = require('firebase-functions'); // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const admin = require("firebase-admin");
 
-// exports.date = functions.https.onRequest((req, res) => {
-//     if (req.method === 'POST') {
-//         res.status(403).send('Forbidden!');
-//     }
-// });
+exports.timestamps = functions.https.onRequest((req, res) => {
 
-console.log("test")
+    var dataModel = {
+        id: "001",
+        name: "TAN"
+    }
+
+    if (req.method === 'GET') {
+        res.status(200).send(dataModel);
+    }
+    else if (req.method === 'POST') {
+        res.status(200).send("is http POST");
+    }
+    else if (req.method === 'PUT') {
+        res.status(200).send("is http PUT");
+    }
+    else if (req.method === 'DELETE') {
+
+        let itemId = req.param('itemId')
+        if (itemId != undefined)
+            res.status(200).send(itemId);
+        else
+            res.status(200).send("ItemId Not found")
+    }
+
+});
